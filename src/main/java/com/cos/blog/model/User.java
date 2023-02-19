542c6112f5bf406187f46f855ebe068df9e67a8f
황인찬 <ghkddlscks19@naver.com>
2023-02-19 15:17:04 +0900

    2023.02.19 feat User 테이블 생성완료

---
+++ src/main/java/com/cos/blog/model/User.java
@@ -0,0 +1,31 @@
+package com.cos.blog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+// ORM -> JAVA(다른언어) Object -> 테이블로 매핑해주는 기술
+@Entity // User 클래스가 MySQL에 테이블이 생성이 된다.
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 데이터베이스의 넘버링 전략을 따라간다.
+    private int id; // 시퀀스, auto_increment
+
+    @Column(nullable = false, length = 30)
+    private String username; // 아이디
+
+    @Column(nullable = false, length = 100) // 123456 => 해쉬(암호화)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @ColumnDefault("'user'")
+    private String role; // Enum을 쓰는게 좋음. // admin, user, manager
+
+    @CreationTimestamp // 시간 자동 입력
+    private Timestamp createDate;
+}
